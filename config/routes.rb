5cc841d5da9357196ec1b005aeb4758cdb44928b
derekfan <haoping.fan@gmail.com>
2017-09-04 14:00:37 +0800

    build group CRUD function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'groups#index'
-  resources :groups, only: :index
+  resources :groups
 end
