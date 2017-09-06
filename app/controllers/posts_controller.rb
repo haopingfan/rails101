d57b03383683cfce4cdff1b1a23dcb5a3c1e8f13
derekfan <haoping.fan@gmail.com>
2017-09-06 16:37:17 +0800

    build post CRUD

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+  def new
+    @group = Group.find(params[:group_id])
+    @post = Post.new
+  end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.new(post_params)
+    @post.user = current_user
+
+    if @post.save
+      redirect_to group_path(@group), notice: '文章 新建成功!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+end
