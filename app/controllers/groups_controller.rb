e08783fa59098363de4ec52d47a9f964599d70f1
derekfan <haoping.fan@gmail.com>
2017-09-06 16:37:17 +0800

    build post CRUD

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,7 +21,9 @@ class GroupsController < ApplicationController
     end
   end
 
-  def show; end
+  def show
+    @posts = @group.posts.recent.paginate(page: params[:page], per_page: 5)
+  end
 
   def edit; end
 
