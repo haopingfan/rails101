b09ca9503beb9b16646cb0f9fafbf76dfeed6bb4
derekfan <haoping.fan@gmail.com>
2017-09-08 15:02:47 +0800

    refactor set_group method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def set_group
-    @group = Group.find(params[:id])
+  def set_group(id)
+    @group = Group.find(params[id])
   rescue ActiveRecord::RecordNotFound
     redirect_to groups_path, alert: "The group id doesn't exist."
   end
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :set_group, only: [:show, :edit, :update, :destroy]
+  before_action only: [:show, :edit, :update, :destroy]{ set_group(:id) }
   before_action :authenticate_user!, only: [:new, :create]
   before_action :authenticate_owner, only: [:edit, :update, :destroy]
 
