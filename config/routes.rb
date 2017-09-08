e16d2d71903d59dc613eda4632349912bbc8b565
derekfan <haoping.fan@gmail.com>
2017-09-08 18:12:54 +0800

    change route for rebase

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'groups#index'
   resources :groups do
-    resources :posts, only: :index
+    resources :posts, only: [:new, :create]
+    resources :group_users, only: [:create, :destroy]
   end
-  resources :group_users, only: [:create, :destroy]
   namespace :account do
     resources :groups, only: :index
     resources :posts, only: :index
