98bbda628525ed7456acd0b15a5917c5b1447ebf
derekfan <haoping.fan@gmail.com>
2017-09-08 18:19:24 +0800

    restore route.rb back

--- config/routes.rb
+++ config/routes.rb
@@ -1,1 +1,13 @@
-
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'groups#index'
+  resources :groups do
+    resources :posts, except: [:index, :show]
+    resources :group_users, only: [:create, :destroy]
+  end
+  namespace :account do
+    resources :groups, only: :index
+    resources :posts, only: :index
+  end
+end
