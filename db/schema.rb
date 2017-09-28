cc0c3dbc6a1964947288abbff27b8b2daabf2e24
derekfan <haoping.fan@gmail.com>
2017-09-28 16:31:57 +0800

    refactor

--- db/schema.rb
+++ db/schema.rb
@@ -73,7 +73,6 @@ ActiveRecord::Schema.define(version: 20170925104815) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.string "cellphone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
